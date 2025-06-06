81f380398a316a3a564aa1c575a61c4fd1e37a70
nikita-bushynskyi <nikita.bushynskyi@corpsoft.io>
2025-06-06 15:00:51 +0200

    [UPDATE] Instructions and model updated

--- app/analysis/text.py
+++ app/analysis/text.py
@@ -1,18 +1,18 @@
 from transformers import pipeline, set_seed
 
-generator = pipeline("text-generation", model="gpt2")
+generator = pipeline("text-generation", model="openai-community/gpt2")
 set_seed(42)
 
 def generate(description):
+    print(f"Desc: '{description}'")
+
     prompt = (
-        f"Write a fake news article based on the following image description:\n"
+        f"Write news article based on the following short image description:\n"
         f"'{description}'.\n"
-        f"Do not mention that it is fake news. Keep it as real as possible.\n"
-        f"Include title of article and body.\n"
-        f"Do not add redundant margins and paragraphs. Keep text as tight as possible.\n"
         f"Make it sound dramatic and suspicious. Include made-up details.\n"
         f"---"
     )
 
-    generated = generator(prompt, max_length=2000, num_return_sequences=1, do_sample=True, temperature=0.9)[0]["generated_text"]
+    generated = generator(prompt, max_length=200, num_return_sequences=1, do_sample=True, temperature=0.9)[0]["generated_text"]
+    print(generated)
     return generated.split("---")[-1].strip()
